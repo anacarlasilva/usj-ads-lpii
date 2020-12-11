0b3d6a4ca18192a762fa911a91a11881e6896dbe
anacarlasilva <66519586+anacarlasilva@users.noreply.github.com>
2020-12-11 15:35:17 -0300

    Primeiro Commit

---
+++ src/main/java/br/edu/usj/ads/lpii/helloname/HelloName.java
@@ -0,0 +1,28 @@
+package br.edu.usj.ads.lpii.helloname;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+
+@Controller
+public class HelloName {
+
+    @PostMapping(value="HelloName")
+    public ModelAndView postHelloName(@RequestParam String nome) {
+        ModelAndView modelAndView = new ModelAndView ("hello-name");
+          
+        String bemVindo = "Seja Bem Vindo" + nome + "! ";
+        modelAndView.addObject("mensagem", bemVindo);
+
+
+        return modelAndView;
+    }
+    
+    }
+    
+    
+
